6ae7e903e89cfdd30b95012baafb7f0cf00c68b9
AshMLondon <98660851+AshMLondon@users.noreply.github.com>
2024-11-09 13:26:57 +0000

    streamlit added with date slider

--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
 import requests, json, os
+
 
 from datetime import date,timedelta
 
@@ -21,6 +23,9 @@ class Model():
             self.save_data_from_elexon()
 
         self.df = pd.DataFrame(self.raw_data)
+        self.df['settlementDate'] = pd.to_datetime(self.df['settlementDate'])
+
+        self.df.drop("systemSellPrice",axis=1)
 
     def save_data_from_elexon(self):
         print("NO FILE - HAVING TO LOAD FROM ELEXON")
@@ -87,19 +92,66 @@ class Model():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     model=Model()
-    # model.get_data_from_elexon()
-    # model.load()
     print(model.df.describe())
 
+
+
     #let's try picking only a certain period
-    new_df=model.df.loc[model.df['settlementPeriod']==1]
-    print(new_df)
-    print(new_df.describe())
+    # new_df=model.df.loc[model.df['settlementPeriod']==1]
+    # print(new_df)
+    # print(new_df.describe())
+
+
+
+
+    # x = st.slider("Select a value")
+    # st.write(x, "squared is", x * x)
+
+    first_date=date.fromisoformat(date.isoformat(model.df.min()["settlementDate"]))
+    last_date= date.fromisoformat(date.isoformat(model.df.max()["settlementDate"]))
+
+
+
+    values = st.slider('Select a range of values',first_date, last_date,(first_date,last_date),format="DD-MMM-YYYY")
+    st.write(values)
+    print(values)
+    lower_date_select=values[0] #date.isoformat(values[0])
+    upper_date_select=values[1] #date.isoformat(values[1])
+    # st.write(lower_date_select,upper_date_select)
+
+
+    # print(model.df[0])
+    # print(model.df[19050])
+
+    d1=date.fromisoformat("2024-02-01")
+    d2=date.fromisoformat("2024-03-01")
+
+
+    print("DDD",d1,"   ",d2)
+    model.df = model.df.set_index(['settlementDate'])
+    # selected = model.df.loc[d1:d2]
+    selected = model.df.loc[lower_date_select:upper_date_select]
+    print(selected.describe)
+
+    st.line_chart(selected, y="systemBuyPrice")
+
+
+    # selected=model.df.loc[lower_date_select<model.df['settlementDate']<=upper_date_select]
+    # selected=model.df.loc[2<=int(model.df['settlementPeriod'])<=6]
+
+
+    # selected=model.df.loc[lower_date_select:upper_date_select]
+
+    # print(selected.describe())
+
+
+    # st.line_chart(selected,y="systemSellPrice")
 
     # model.df.to_csv('data_out.csv')
 
-    model.df.plot()
-    plt.show()
+    # quit()
+    # model.df.plot()
+    # plt.show()
 
 
 
